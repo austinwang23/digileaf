cef4cf217600e67b9513551d29de32b9f047e1b6
Zi Zhou Yin <zzyin@uwaterloo.ca>
2023-06-13 17:44:44 -0400

    created home fragment

---
+++ app/src/main/java/com/example/digileaf/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.digileaf
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.digileaf.databinding.ActivityMainBinding
+import nl.joery.animatedbottombar.AnimatedBottomBar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animatedBottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+                when(newTab.id){
+                    R.id.home -> replaceFragment(Home())
+                    R.id.calendar -> replaceFragment(Calendar())
+                }
+            }
+        })
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+}
