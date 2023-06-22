40784299f18020914c1056fa336e9feed59affb2
ZIZHOU YIN <zzyin@uwaterloo.ca>
2023-06-22 16:02:36 -0400

    added more to recycler view

--- app/src/main/java/com/example/digileaf/Home.kt
+++ app/src/main/java/com/example/digileaf/Home.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.digileaf.model.Plant
 
@@ -20,9 +21,15 @@ class Home : Fragment() {
 
         //Initialize plants
         plantList = ArrayList()
-        plantList.add(Plant("Timothy", "2 days old", "Timothy is questionable", R.drawable.image_1))
-        plantList.add(Plant("Johnny", "1 days old", "Johnny is <3", R.drawable.image_2))
-        plantList.add(Plant("Carla", "4 days old", "Carla is *****", R.drawable.image_3))
+        plantList.add(Plant("Timothy", "2 days", "Spiky Plant", R.drawable.image_1))
+        plantList.add(Plant("Johnny", "1 days", "Pretty Plant", R.drawable.image_2))
+        plantList.add(Plant("Carla", "2 mos", "Ugly Plant", R.drawable.image_3))
+        plantList.add(Plant("Timothy", "2 days", "Spiky Plant", R.drawable.image_1))
+        plantList.add(Plant("Johnny", "1 days", "Pretty Plant", R.drawable.image_2))
+        plantList.add(Plant("Carla", "2 mos", "Ugly Plant", R.drawable.image_3))
+        plantList.add(Plant("Timothy", "2 days", "Spiky Plant", R.drawable.image_1))
+        plantList.add(Plant("Johnny", "1 days", "Pretty Plant", R.drawable.image_2))
+        plantList.add(Plant("Carla", "2 mos", "Ugly Plant", R.drawable.image_3))
 
         plantAdapter = PlantAdapter(plantList)
     }
@@ -35,11 +42,11 @@ class Home : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
         // Initialize the RecyclerView
-        recyclerView = view.findViewById(R.id.home_RV)
+        recyclerView = view.findViewById(R.id.plant_RV)
 
         // Set up the RecyclerView with the adapter and layout manager
         recyclerView.adapter = plantAdapter
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         return view
     }
--- app/src/main/java/com/example/digileaf/PlantAdapter.kt
+++ app/src/main/java/com/example/digileaf/PlantAdapter.kt
@@ -12,8 +12,10 @@ class PlantAdapter(private val plantList: ArrayList<Plant>) :
     RecyclerView.Adapter<PlantAdapter.PlantViewHolder>() {
 
     class PlantViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.plant_item_image)
-        val textView: TextView = itemView.findViewById(R.id.plant_item_name)
+        val plantImageView: ImageView = itemView.findViewById(R.id.plant_item_image)
+        val plantNameView: TextView = itemView.findViewById(R.id.plant_item_name)
+        val plantDescriptionView: TextView = itemView.findViewById(R.id.plant_item_description)
+        val plantAgeView: TextView = itemView.findViewById(R.id.plant_item_age)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantViewHolder {
@@ -28,7 +30,9 @@ class PlantAdapter(private val plantList: ArrayList<Plant>) :
 
     override fun onBindViewHolder(holder: PlantViewHolder, position: Int) {
         val plant = plantList[position]
-        holder.imageView.setImageResource(plant.plantImage)
-        holder.textView.text = plant.plantName
+        holder.plantImageView.setImageResource(plant.plantImage)
+        holder.plantNameView.text = plant.plantName
+        holder.plantDescriptionView.text = plant.plantDescription
+        holder.plantAgeView.text = plant.plantAge
     }
 }
