5e4ea5bb1eb5a818b1c5e879ede917c1aca1fb9d
Austin Wang <a242wang@uwaterloo.ca>
2023-06-23 02:05:59 -0400

    Added upload image feature

--- app/src/main/java/com/example/digileaf/AddPlantActivity.kt
+++ app/src/main/java/com/example/digileaf/AddPlantActivity.kt
@@ -3,13 +3,22 @@ package com.example.digileaf
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class AddPlantActivity : AppCompatActivity() {
+    private lateinit var selectImageButton: Button
+    private lateinit var imageView: ImageView
 
     private lateinit var plantName : EditText
     private lateinit var plantSpecies : EditText
     private lateinit var plantDescription : EditText
 
+    private val PICK_IMAGE_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
@@ -17,7 +26,22 @@ class AddPlantActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         }
         setContentView(R.layout.activity_add_plant)
+        selectImageButton = findViewById(R.id.select_image_button)
+        imageView = findViewById(R.id.image_view)
 
+        selectImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            val imageUri = data.data
+            val imageBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri!!)) // prob handle this null better
+            imageView.setImageBitmap(imageBitmap)
+        }
         plantName = findViewById(R.id.add_plant_title)
 
 //        val plant = intent.getParcelableExtra<Plant>("plant")
