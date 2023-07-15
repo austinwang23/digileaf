25d086d2f1f439b91efa9a123580c8a9f41468cb
Alan Hu <alanhu@Alans-MacBook-Air.local>
2023-07-14 22:43:29 -0400

    update notification icon

--- app/src/main/java/com/example/digileaf/Notification.kt
+++ app/src/main/java/com/example/digileaf/Notification.kt
@@ -15,8 +15,7 @@ const val messageExtra = "messageExtra"
 class Notification : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val notification = NotificationCompat.Builder(context, channelID)
-//            .setSmallIcon(R.drawable.ic_reminder)
-            .setSmallIcon(R.drawable.bg_circle)
+            .setSmallIcon(R.drawable.ic_reminder)
             .setContentTitle(intent.getStringExtra(titleExtra))
             .setContentText(intent.getStringExtra(messageExtra))
             .build()
